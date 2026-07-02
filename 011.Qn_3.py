ff28a39865de666bd4a61ab88154d98c7e6bab2c
SARULATHA MURUGESAN <146159104+Sarulatha2004@users.noreply.github.com>
2026-07-02 11:22:06 +0530

    Add files via upload

---
+++ 011.Qn_3.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+
+mul=a*b*c
+add=a+b+c
+div=mul/add
+print("The result:",div)
---
+++ 012.Fibanocci_series.py
@@ -0,0 +1,6 @@
+a=0
+b=1
+print("Fibanocci series:")
+while a<10:
+    print("\n",a)
+    a,b=b,a+b
